54800bf06af438f5dc38126a76eb3417dabdb059
Ricardo Rubio <rickarubio@gmail.com>
2013-06-30 23:23:01 -0600

    Updated ex04.rb to show a more efficient way of handling user input to float conversion.

--- 1wk/ex04.rb
+++ 1wk/ex04.rb
@@ -4,10 +4,15 @@
   there are in a year (do not bother right now about leap years etc.).
 =end
 puts "Enter any number of years: "
-years = gets.chomp.to_f
+# Using chomp after gets in this case is inefficient because:
+# .to_f Returns the result of interpreting leading characters in str 
+# as a floating point number. Extraneous characters past the end 
+# of a valid number are ignored.
+# years = gets.chomp.to_f 
+years = gets.to_f
 until years > 0
   puts "Invalid input, please try again."
   puts "Enter any number of years: "
-  years = gets.chomp.to_f
+  years = gets.to_f
 end
 puts "There are #{years * 365 * 24 * 60} minutes in #{years} year(s)"
