73d5185c7b2de6e1426f0047d0c29a6924749720
Ricardo Rubio <rickarubio@gmail.com>
2013-06-30 23:36:52 -0600

    Questioning my initial hypothesis of Ruby name resolution conflicts

--- 1wk/ex05.rb
+++ 1wk/ex05.rb
@@ -15,3 +15,13 @@ puts my_string
 # this is how you tell ruby you want the method instead of the variable
 # with the same name:
 puts my_string()
+
+# Correction to the above:
+# It seems like Ruby must have some other way of figuring out which name
+# it chooses when the same name is used for different things.
+# My guess is scope, maybe Ruby is going after the most accessible scope 1st.
+def string2
+  'What is going on here?'
+end
+string2 = 'which string comes first?'
+puts string2
