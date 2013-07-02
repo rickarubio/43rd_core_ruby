7dfa678bc7017b6f4e29ea8934568aed9aca1f81
Ricardo Rubio <rickarubio@gmail.com>
2013-07-01 18:07:34 -0600

    added rubocop log and began work on ch02.rb

---
+++ 1wk/ch02.rb
@@ -0,0 +1,38 @@
+# https://github.com/rickarubio
+=begin
+  Write a program that asks for user input, then changes that input from one temperature unit to another based upon the user's wishes.
+
+  Write code to handle user input. That code must include a succinct explanation of how the user should type the values so the program can understand if it's meant to convert from Celsius to Fahrenheit or vice-versa. It will then proceed to "parse" the input and obtain two "values": "conversion to make" and "value to use", where the first means to convert from Celsius to Fahrenheit or vice-versa and the second is the temperature value to be converted.
+
+  Write DRY (Don't Repeat Yourself) code - method(s) - for the conversion process, assuming that the temperature value is always passed as a Float. This is to "prevent" you from writing code to validate the argument. But that doesn't mean you shouldn't validate the value. Meaning that you shouldn't worry if the argument is or is not a Float - assume it is - and instead worry if the value is within an "acceptable range". Consider absolute zero.
+
+  If you want to take this challenge even further, then make the conversion program also able to handle other units of temperature (such as Kelvin, Rankine, etc).
+
+  Note: the intention here is not to have you code lines and lines of statements but to think in out-of-the-box, abstract ways:
+
+  Tip 1: Kelvin is just an "adjustment" of another calculation. ;-)
+  Tip 2: A conversion of a value can be seen as unit_from, unit_to, value_to_convert.
+=end
+require 'rubygems'
+gem "ruby-units", "~> 1.4.3"
+
+def welcome_message
+  puts "Welcome to temperature unit converter v1.0"
+  puts "Currently supports conversions between these units:"
+  puts "(C)elsius, (F)ahrenheit, (K)elvin, and (R)ankine"
+  puts "Examples of valid conversion requests:"
+  puts "50 C to F"
+  puts "-14F to C"
+  puts "299.817      K to R"
+  puts "etc..."
+end
+
+welcome_message()
+print "Enter your temperature conversion request:"
+conversion_request = gets.chomp
+
+
+
+
+
+
